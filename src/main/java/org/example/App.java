1a2725f1521ecf7c53369b0f27c9f862a38c4f80
darkstar100x <darkstar100x@gmail.com>
2021-10-12 17:17:41 -0400

    Second commit: Added tests

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,8 +3,30 @@ package org.example;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println("Test: " + Palindromes.generateLPS("character"));
+        String[] pals = {"madam", "civic", "aibohphobia"};
+        String[] containPals = {"character", "BBABCBCAB", "widow", "never", "restock paper"};
+        String[] noPals = {"Save", "Load"};
+        System.out.println("For known palindromes:");
+        for (String in: pals) {
+            printLPS(in);
+        }
+        System.out.println();
+        System.out.println("For strings containing palindromes:");
+        for (String in: containPals) {
+            printLPS(in);
+        }
+        System.out.println();
+        System.out.println("For strings without palindromes:");
+        for (String in: noPals) {
+            printLPS(in);
+        }
+    }
+
+    public static void printLPS(String input) {
+        System.out.print("The longest palindrome subsequence in " + input + " is: ");
+        System.out.println(Palindromes.generateLPS(input));
+        System.out.println("Length: " + Palindromes.lengthLPS(input));
     }
 }
